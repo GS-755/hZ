cd2ed08ee0adde27565091e7c54e1af9b83cf82f
GS-755 <21dh114236@st.huflit.edu.vn>
2023-08-09 20:29:24 +0700

    Update & init second project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hZ
+# hZ v3.7.2
 Simple Python3 module to decrease monitor's refresh rate
---
+++ app.py
@@ -0,0 +1,22 @@
+from os import system
+from time import sleep
+import psutil as ps 
+
+MAX_FREQ = 144
+MIN_FREQ = 60
+
+def set_freq(hzValue) -> None:
+  system(f"runtime\\qres1095\\qres.exe f={str(hzValue)}")
+
+def main() -> None:
+  while(True):
+    battery = ps.sensors_battery()
+    plugged = battery.power_plugged
+    if(plugged == False):
+      set_freq(MIN_FREQ)
+    set_freq(MAX_FREQ)
+    sleep(0.2)
+
+
+
+
